f78989fe03011c6e6568d229e71f685eeaae6a1b
Ben Wiedermann <benw@cs.hmc.edu>
2023-02-22 13:05:49 -0800

    small changes

--- for_loop/src/main/scala/ForLoop.scala
+++ for_loop/src/main/scala/ForLoop.scala
@@ -1,9 +1,9 @@
 // define the new control-flow structure here
 
+var i = 0;
+
 @main
 def main =
-
-  var i = 0;
-  for_loop(i = 0)(i < 10)(i += 2) {
+  for_loop((i = 0))(i < 10)(i += 2) {
     println(i);
   }
